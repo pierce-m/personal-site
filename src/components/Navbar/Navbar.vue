71e0f5ccad3389d3ee7d5ee0cee919d96e8967ab
Pierce McEntagart <pierce@codecov.io>
2019-12-25 16:48:06 -0800

    Name change, tensleep photo.

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,48 +1,28 @@
 <template>
   <v-app-bar :app="app">
-      <v-menu offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn
-            text
-            v-on="on"
-          >
-            Projects
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in projects"
-            :key="index"
-          >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-
-      <v-spacer />
-
-      <v-toolbar-items>
-        <v-btn text to="/">
-          Home
-        </v-btn>
-        <v-btn text to="/about">
-          About
-        </v-btn>
-        <v-btn
-          icon
-          :href="EXTERNAL_URLS.LINKEDIN"
-          color="#0077b5"
-        >
-          <v-icon large>mdi-linkedin-box</v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          :href="EXTERNAL_URLS.GITHUB"
-          color="black"
-        >
-          <v-icon large>mdi-github-circle</v-icon>
-        </v-btn>
-      </v-toolbar-items>
+    <v-spacer />
+    <v-toolbar-items>
+      <v-btn text to="/">
+        Home
+      </v-btn>
+      <v-btn text to="/about">
+        About
+      </v-btn>
+      <v-btn
+        icon
+        :href="EXTERNAL_URLS.LINKEDIN"
+        color="#0077b5"
+      >
+        <v-icon large>mdi-linkedin-box</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        :href="EXTERNAL_URLS.GITHUB"
+        color="black"
+      >
+        <v-icon large>mdi-github-circle</v-icon>
+      </v-btn>
+    </v-toolbar-items>
   </v-app-bar>
 </template>
 
@@ -52,13 +32,8 @@ import { EXTERNAL_URLS } from '../../utils/constants.js'
 export default {
   name: "navbar",
   props: ['app'],
-  data: () => {
-    return {
-      projects: [
-        { title: "The Buzz" }
-      ],
-      EXTERNAL_URLS: EXTERNAL_URLS
-    }
-  }
+  data: () => ({
+    EXTERNAL_URLS: EXTERNAL_URLS
+  })
 }
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,23 @@
 <template>
-  <v-img :src="require('../assets/brain.jpg')" max-height="800"/>
+  <v-carousel cycle height="830">
+    <v-carousel-item
+      v-for="(image, index) in carouselImages"
+      :key="index"
+    >
+      <v-img :src="require('../assets/' + image)" max-height="830"/>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => ({
+    carouselImages: [
+      'brain.jpg',
+      'tensleep.jpg'
+    ]
+  })
 }
 
 </script>
